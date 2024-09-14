b8c194a1582ee5438d6e68fb8a9e43b5ce073368
Johnnykoch02 <johnnykoch02@gmail.com>
2024-09-14 13:18:55 -0400

    fix training script

--- scripts/train_dynamix_critiq.sh
+++ scripts/train_dynamix_critiq.sh
@@ -2,9 +2,9 @@
 
 source scripts/setup_env.sh
 
-python '/home/blazekin/dev/ControlDrop-RPAL/control_dropping/src/training/training_dynamix.py' \
+python "$CONTROL_DROP_DIR/control_dropping/src/training/training_dynamix.py" \
   --name "dynamix_critiq_training-9-01-2024" \
-  --batch_size 512 \
+  --batch_size 256 \
   --epochs 500 \
   --warmup_epochs 10 \
   --num_workers 12 \
